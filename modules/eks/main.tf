b853538e00d51ed8397b6c862c953776d0fd8087
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-07 10:11:36 +0200

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -9,9 +9,62 @@ data "aws_eks_cluster_auth" "this" {
   name = aws_eks_cluster.this.name
 }
 
+# IAM Role para el control plane de EKS
+resource "aws_iam_role" "cluster" {
+  name = "${var.cluster_name}-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.cluster_assume.json
+}
+
+data "aws_iam_policy_document" "cluster_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_attach" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# IAM Role para los worker nodes
+resource "aws_iam_role" "node" {
+  name = "${var.cluster_name}-node-role"
+  assume_role_policy = data.aws_iam_policy_document.node_assume.json
+}
+
+data "aws_iam_policy_document" "node_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "node_attach1" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+resource "aws_iam_role_policy_attachment" "node_attach2" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+resource "aws_iam_role_policy_attachment" "node_attach3" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# El cluster
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
-  role_arn = var.cluster_role_arn
+  role_arn = aws_iam_role.cluster.arn
 
   vpc_config {
     subnet_ids         = var.subnet_ids
@@ -22,7 +75,7 @@ resource "aws_eks_cluster" "this" {
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = var.node_group_name
-  node_role_arn   = var.node_role_arn
+  node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = var.subnet_ids
 
   scaling_config {
@@ -30,5 +83,6 @@ resource "aws_eks_node_group" "this" {
     max_size     = var.node_count + 1
     min_size     = var.node_count
   }
+
   instance_types = [var.node_instance_type]
 }
