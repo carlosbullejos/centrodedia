9081204463859bfee6fe42d90a679e984bac8e5b
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-07 14:15:46 +0200

    Update variables.tf

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -69,6 +69,10 @@ variable "public_subnet_cidrs" {
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
+variable "public_subnet_azs" {
+  type    = list(string)
+  default = ["us-east-1a","us-east-1b","us-east-1c"]
+}
 
 # Tanto el cluster como el node‑group usarán este mismo LabRole
 variable "cluster_role_arn" {
