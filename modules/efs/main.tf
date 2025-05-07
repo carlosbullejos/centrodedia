d7cdca1296ca695ea5be63576ddbceca2da683ea
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-07 10:18:08 +0200

    Update main.tf

--- modules/efs/main.tf
+++ modules/efs/main.tf
@@ -7,9 +7,19 @@ resource "aws_efs_file_system" "this" {
   }
 }
 
+# Mapeamos cada CIDR (estático) al ID de subnet correspondiente (dinámico)
+locals {
+  subnet_map = {
+    for cidr in var.public_subnet_cidrs :
+    cidr => var.subnet_ids[index(var.public_subnet_cidrs, cidr)]
+  }
+}
+
 resource "aws_efs_mount_target" "this" {
-  for_each         = toset(var.subnet_ids)
-  file_system_id   = aws_efs_file_system.this.id
-  subnet_id        = each.value
-  security_groups  = [var.security_group_id]
+  # keys (cidr) son conocidas al plan; values (subnet IDs) se llenan en apply
+  for_each        = local.subnet_map
+
+  file_system_id  = aws_efs_file_system.this.id
+  subnet_id       = each.value
+  security_groups = [var.security_group_id]
 }
