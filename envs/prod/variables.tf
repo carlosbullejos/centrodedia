47f71fd41d0c7e1d6d0afdd249605f4ab402904a
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-07 10:00:26 +0200

    Update variables.tf

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -4,41 +4,59 @@ variable "aws_region" {
 }
 
 variable "cluster_name" {
-  type = string
+  description = "Nombre del cluster EKS"
+  type        = string
 }
-variable "vpc_id" {
-  type = string
+
+variable "my_ip_cidr" {
+  description = "Rango CIDR para acceso SSH a EC2"
+  type        = string
 }
-variable "subnet_ids" {
-  type = list(string)
-}
+
 variable "node_group_name" {
   type    = string
   default = "worker-nodes"
 }
+
 variable "node_count" {
   type    = number
   default = 2
 }
+
 variable "node_instance_type" {
   type    = string
   default = "t3.medium"
 }
+
 variable "efs_name" {
   type    = string
   default = "app-efs"
 }
+
 variable "ami_id" {
-  type = string
+  description = "AMI ID para la instancia EC2"
+  type        = string
 }
+
 variable "instance_type" {
   type    = string
   default = "t3.micro"
 }
+
 variable "backup_bucket_name" {
-  type    = string
-  default = "carlosbullejos-copiasdeseguridad"
+  description = "Nombre del bucket S3 para backups"
+  type        = string
+  default     = "carlosbullejos-copiasdeseguridad"
 }
-variable "my_ip_cidr" {
-  type = string
+
+variable "cidr" {
+  description = "CIDR principal de la VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
