a4b693b6cd0b3e37d807917653ae2867e76a9724
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-07 10:00:03 +0200

    Update main.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,6 +1,13 @@
+module "network" {
+  source              = "../../modules/network"
+  cidr                = var.cidr
+  public_subnet_cidrs = var.public_subnet_cidrs
+  tags                = { Environment = "prod" }
+}
+
 module "security" {
-  source  = "../../modules/security"
-  vpc_id       = var.vpc_id
+  source       = "../../modules/security"
+  vpc_id       = module.network.vpc_id
   cluster_name = var.cluster_name
   my_ip_cidr   = var.my_ip_cidr
 }
@@ -8,8 +15,8 @@ module "security" {
 module "eks" {
   source             = "../../modules/eks"
   cluster_name       = var.cluster_name
-  vpc_id             = var.vpc_id
-  subnet_ids         = var.subnet_ids
+  vpc_id             = module.network.vpc_id
+  subnet_ids         = module.network.subnet_ids
   node_group_name    = var.node_group_name
   node_count         = var.node_count
   node_instance_type = var.node_instance_type
@@ -19,7 +26,7 @@ module "eks" {
 module "efs" {
   source            = "../../modules/efs"
   name              = var.efs_name
-  subnet_ids        = var.subnet_ids
+  subnet_ids        = module.network.subnet_ids
   security_group_id = module.security.efs_sg_id
 }
 
@@ -27,7 +34,7 @@ module "ec2" {
   source            = "../../modules/ec2"
   ami_id            = var.ami_id
   instance_type     = var.instance_type
-  subnet_id         = var.subnet_ids[0]
+  subnet_id         = module.network.subnet_ids[0]
   security_group_id = module.security.ec2_app_sg_id
   instance_name     = "app-server"
   root_volume_size  = 20
@@ -36,6 +43,8 @@ module "ec2" {
 }
 
 # Data source for existing S3 bucket
+
 data "aws_s3_bucket" "backup" {
   bucket = var.backup_bucket_name
 }
+```
