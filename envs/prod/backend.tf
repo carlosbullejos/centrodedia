ce3e78434eecc8ffb4edbeaac914ac75587d5ac7
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-07 09:14:47 +0200

    Update backend.tf

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "carlosbullejos-copiasdeseguridad"
-    key            = "eks-prod/state.tfstate"
-    region         = "us-east-1"          
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket       = "carlosbullejos-copiasdeseguridad"
+    key          = "eks-prod/state.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
