78641b742d7bf23c4f0fd45ceb91ca28fa718635
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-07 09:57:03 +0200

    Create variables.tf

---
+++ modules/network/variables.tf
@@ -0,0 +1,17 @@
+variable "cidr" {
+  description = "CIDR principal de la VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "Lista de CIDRs para subnets públicas"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "tags" {
+  description = "Tags comunes"
+  type        = map(string)
+  default     = { Environment = "prod" }
+}
