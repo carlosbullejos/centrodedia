59c9fe9c843408bf65ab8687d89fcb6b6c0f3497
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-08 10:39:12 +0200

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,64 +1,23 @@
-provider "aws" {}
-
-# 1) SG para EKS worker nodes
-resource "aws_security_group" "eks_nodes" {
-  name        = "${var.cluster_name}-nodes-sg"
-  description = "SG para worker nodes de EKS"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    self        = true
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# 2) SG para mount targets de EFS
+# SG de EFS
 resource "aws_security_group" "efs" {
-  name        = "${var.cluster_name}-efs-sg"
-  description = "SG para mount targets de EFS"
-  vpc_id      = var.vpc_id
-
+  # …
   ingress {
-    from_port             = 2049
-    to_port               = 2049
-    protocol              = "tcp"
-    security_groups       = [aws_security_group.ec2_app.id]
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_app.id]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
 }
 
-# 3) SG para tu instancia EC2 (app-server)
+# SG de EC2 sólo SSH en 22, sin NFS
 resource "aws_security_group" "ec2_app" {
-  name        = "${var.cluster_name}-app-sg"
-  description = "SG para instancia EC2 app-server"
-  vpc_id      = var.vpc_id
-
+  # …
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [var.my_ip_cidr]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
 }
