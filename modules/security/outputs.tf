3a48b530c1c5cf6d73ac9bab1423951a64367b6e
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-08 10:42:57 +0200

    Update outputs.tf

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -1,12 +1,14 @@
 output "eks_nodes_sg_id" {
+  description = "Security Group ID for EKS worker nodes"
   value       = aws_security_group.eks_nodes.id
-  description = "Security group ID for EKS worker nodes"
 }
+
 output "efs_sg_id" {
+  description = "Security Group ID for EFS mount targets"
   value       = aws_security_group.efs.id
-  description = "Security group ID for EFS mount targets"
 }
+
 output "ec2_app_sg_id" {
+  description = "Security Group ID for EC2 application server"
   value       = aws_security_group.ec2_app.id
-  description = "Security group ID for EC2 application instance"
 }
