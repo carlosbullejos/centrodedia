28cd3d23b62d2f8452f746aa351acdc99734a490
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-08 09:47:45 +0200

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "app" {
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   associate_public_ip_address = true      # ← AQUÍ
-
+  key_name = var.ssh_key_name
   vpc_security_group_ids = [var.security_group_id]
   tags                   = { Name = var.instance_name }
 
