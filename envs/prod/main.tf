802d357e53afb4b5996251bd02a5a8c09687676a
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-09 20:25:28 +0200

    Update main.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,3 +1,27 @@
+###############################################################################
+# 1) Providers
+###############################################################################
+provider "aws" {
+  region = var.region
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+###############################################################################
+# 2) Módulos de Infra
+###############################################################################
 module "network" {
   source              = "../../modules/network"
   cidr                = var.cidr
@@ -6,12 +30,19 @@ module "network" {
   tags                = { Environment = "prod" }
 }
 
-
 module "security" {
   source       = "../../modules/security"
   vpc_id       = module.network.vpc_id
   cluster_name = var.cluster_name
   my_ip_cidr   = var.my_ip_cidr
+}
+
+module "efs" {
+  source            = "../../modules/efs"
+  name              = var.efs_name
+  subnet_ids        = module.network.subnet_ids
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  security_group_id = module.security.efs_sg_id
 }
 
 module "eks" {
@@ -27,50 +58,11 @@ module "eks" {
   cluster_role_arn   = var.cluster_role_arn
   node_role_arn      = var.node_role_arn
 
-  # Estos flags replican “Public access” y “Modo automático desactivado”:
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = false
   cluster_public_access_cidrs     = ["0.0.0.0/0"]
-  ssh_key_name             = var.ssh_key_name
-  node_security_group_ids  = [ module.security.ec2_app_sg_id ]
-}
-# 1) Autenticación EKS
-data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
-}
-
-# 2) Providers Kubernetes & Helm
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
-
-# 3) Extraer el nombre del IAM Role de nodos desde su ARN
-locals {
-  node_role_name = replace(
-    var.node_role_arn,
-    "arn:aws:iam::${var.aws_account}:role/",
-    ""
-  )
-}
-
-
-
-module "efs" {
-  source               = "../../modules/efs"
-  name                 = var.efs_name
-  subnet_ids           = module.network.subnet_ids
-  public_subnet_cidrs  = var.public_subnet_cidrs
-  security_group_id    = module.security.efs_sg_id
+  ssh_key_name                    = var.ssh_key_name
+  node_security_group_ids         = [module.security.ec2_app_sg_id]
 }
 
 module "ec2" {
@@ -83,12 +75,69 @@ module "ec2" {
   root_volume_size  = 20
   efs_id            = module.efs.efs_id
   efs_mount_point   = "/mnt/efs"
-  ssh_key_name      = var.ssh_key_name 
-  depends_on = [
-     module.efs
-     ]
+  ssh_key_name      = var.ssh_key_name
+  depends_on        = [module.efs]
+}
+
+###############################################################################
+# 3) Data Sources
+###############################################################################
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
 }
 
 data "aws_s3_bucket" "backup" {
   bucket = var.backup_bucket_name
+}
+
+###############################################################################
+# 4) Recursos K8s / NFS Provisioner
+###############################################################################
+# 4.1 Permitir tráfico NFS (2049) desde nodos EKS al EFS
+resource "aws_security_group_rule" "allow_nfs_from_eks" {
+  description              = "Permitir montaje NFS (2049) desde EKS"
+  type                     = "ingress"
+  from_port                = 2049
+  to_port                  = 2049
+  protocol                 = "tcp"
+
+  security_group_id        = module.security.efs_sg_id
+  source_security_group_id = module.security.eks_nodes_sg_id
+}
+
+# 4.2 Instalar el NFS subdir external provisioner
+resource "helm_release" "nfs_subdir_provisioner" {
+  name       = "nfs-subdir-external-provisioner"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/"
+  chart      = "nfs-subdir-external-provisioner"
+  version    = "4.0.11"
+
+  set {
+    name  = "nfs.server"
+    value = "${module.efs.efs_id}.efs.${var.region}.amazonaws.com"
+  }
+  set {
+    name  = "nfs.path"
+    value = "/"
+  }
+  set {
+    name  = "storageClass.name"
+    value = "nfs-client"
+  }
+  set {
+    name  = "storageClass.defaultClass"
+    value = "true"
+  }
+  set {
+    name  = "storageClass.archiveOnDelete"
+    value = "false"
+  }
+
+  timeout         = 300
+  atomic          = true
+  cleanup_on_fail = true
+
+  # Aseguramos que la release sólo se aplica después de que el clúster EKS esté listo
+  depends_on = [module.eks]
 }
