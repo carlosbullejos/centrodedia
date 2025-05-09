27c549a53cbc5d91ee7679602cc54606a1a92c4f
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-09 20:43:41 +0200

    Update main.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,23 +1,9 @@
 ###############################################################################
 # 1) Providers
 ###############################################################################
-provider "aws" {
-  region = var.region
-}
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
 
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
+
 
 ###############################################################################
 # 2) Módulos de Infra
