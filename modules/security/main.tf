0b25efd3f1d1c34caffe2b9cd70d8f3290c58d8e
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-13 08:14:04 +0200

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "ec2_app" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
