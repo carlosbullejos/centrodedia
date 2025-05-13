ff65abb0a0c13485aa442b36ad72776722cafa32
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-13 08:34:01 +0200

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -6,11 +6,21 @@ resource "aws_security_group" "eks_nodes" {
   description = "SG para worker nodes de EKS"
   vpc_id      = var.vpc_id
 
+  # Permitir todo entre nodos
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     self        = true
+  }
+
+  # Permitir NFS desde EFS
+  ingress {
+    description              = "NFS desde EFS"
+    from_port                = 2049
+    to_port                  = 2049
+    protocol                 = "tcp"
+    security_groups          = [aws_security_group.efs.id]
   }
 
   egress {
@@ -27,11 +37,22 @@ resource "aws_security_group" "efs" {
   description = "SG para mount targets de EFS"
   vpc_id      = var.vpc_id
 
+  # Permitir NFS desde nodos EKS
   ingress {
-    from_port       = 2049
-    to_port         = 2049
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_app.id]
+    description              = "NFS desde EKS"
+    from_port                = 2049
+    to_port                  = 2049
+    protocol                 = "tcp"
+    security_groups          = [aws_security_group.eks_nodes.id]
+  }
+
+  # (Opcional) Permitir NFS desde tu instancia EC2
+  ingress {
+    description              = "NFS desde EC2 app-server"
+    from_port                = 2049
+    to_port                  = 2049
+    protocol                 = "tcp"
+    security_groups          = [aws_security_group.ec2_app.id]
   }
 
   egress {
@@ -53,6 +74,15 @@ resource "aws_security_group" "ec2_app" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Permitir NFS desde EFS (si vas a montar EFS manualmente aquí)
+  ingress {
+    description = "NFS desde EFS"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    security_groups = [aws_security_group.efs.id]
   }
 
   egress {
