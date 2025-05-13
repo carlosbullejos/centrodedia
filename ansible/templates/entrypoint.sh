0ebbaeebb044e4b6c4a1ae797b2d91b61e66a526
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-13 11:38:55 +0200

    Update entrypoint.sh

--- ansible/templates/entrypoint.sh
+++ ansible/templates/entrypoint.sh
@@ -11,8 +11,8 @@ mkdir -p /home/"$FTP_USER" \
          /var/run/vsftpd/empty \
          /var/run/vsftpd
 chown -R "$FTP_USER":"$FTP_USER" /home/"$FTP_USER" /home/ftpusers /var/run/vsftpd
-chmod 755 /home/"$FTP_USER" /var/run/vsftpd \
-        775 /home/ftpusers /home/ftpusers/shared
+chmod 755 /home/"$FTP_USER" /var/run/vsftpd 
+chmod 775 /home/ftpusers /home/ftpusers/shared
 
 # Ejecuta el demonio
 exec "$@"
