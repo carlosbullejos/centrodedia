d85d60acc413a01f899df3faa740ca672df40959
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-14 08:45:24 +0200

    Update main.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -99,10 +99,7 @@ resource "aws_instance" "app_server" {
       cluster_name    = var.cluster_name
       region          = var.region
       efs_id          = module.efs.efs_id
-      efs_mount_point = "/mnt/efs"
-      aws_access_key_id     = var.aws_access_key_id      
-      aws_secret_access_key = var.aws_secret_access_key   
-      aws_session_token     = var.aws_session_token      
+      efs_mount_point = "/mnt/efs"   
     }
   )
 
