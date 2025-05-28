c7bdcf4e12f14a44dfdce828bb2948aae7d4a730
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-28 14:31:51 +0200

    Update main.tf

--- modules/efs/main.tf
+++ modules/efs/main.tf
@@ -29,7 +29,7 @@ resource "aws_efs_mount_target" "this" {
 # 4) Renderizado "on-the-fly" de tu StorageClass + PV de Kubernetes
 locals {
   storageclass_manifest = templatefile(
-    "${path.module}/kubernetes/nfs-storageclass.yaml",
+    "/kubernetes/nfs-storageclass.yaml",
     {
       efs_id = aws_efs_file_system.this.id
     }
