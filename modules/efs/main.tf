1c48365362c216d89cff79e9fa58e961d5fe6be8
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-05-28 14:27:40 +0200

    Update main.tf

--- modules/efs/main.tf
+++ modules/efs/main.tf
@@ -1,5 +1,8 @@
+# modules/efs/main.tf
+
 provider "aws" {}
 
+# 1) Tu EFS
 resource "aws_efs_file_system" "this" {
   creation_token = var.name
   lifecycle_policy {
@@ -7,7 +10,7 @@ resource "aws_efs_file_system" "this" {
   }
 }
 
-# Mapeamos cada CIDR (estático) al ID de subnet correspondiente (dinámico)
+# 2) Mapear CIDRs a subnets dinámicamente
 locals {
   subnet_map = {
     for cidr in var.public_subnet_cidrs :
@@ -15,11 +18,26 @@ locals {
   }
 }
 
+# 3) Crear mount targets en cada subnet
 resource "aws_efs_mount_target" "this" {
-  # keys (cidr) son conocidas al plan; values (subnet IDs) se llenan en apply
   for_each        = local.subnet_map
-
   file_system_id  = aws_efs_file_system.this.id
   subnet_id       = each.value
   security_groups = [var.security_group_id]
+}
+
+# 4) Renderizado "on-the-fly" de tu StorageClass + PV de Kubernetes
+locals {
+  storageclass_manifest = templatefile(
+    "${path.module}/kubernetes/nfs-storageclass.yaml",
+    {
+      efs_id = aws_efs_file_system.this.id
+    }
+  )
+}
+
+# 5) Exponer el manifiesto como output para tu CI/CD
+output "efs_storageclass_manifest" {
+  description = "YAML del StorageClass + PV con el EFS ID inyectado"
+  value       = local.storageclass_manifest
 }
