0ef6daa94b168ea697d5e83d6de0967e7cec44fc
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-06-03 12:55:12 +0200

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -31,3 +31,24 @@ variable "git_token" {
   type        = string
   sensitive   = true
 }
+variable "aws_access_key_id" {
+ 
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+ 
+  type        = string
+  sensitive   = true
+}
+variable "aws_session_token" {
+ 
+  type        = string
+  sensitive   = true
+}
+variable "aws_region" {
+ 
+  type        = string
+  sensitive   = true
+}
