8181e358971c56892eb4fdf61e5e7207c9e9eb34
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-06-09 11:02:34 +0200

    Update outputs.tf

--- envs/prod/outputs.tf
+++ envs/prod/outputs.tf
@@ -14,3 +14,9 @@ output "ec2_instance_ip" {
 output "backup_bucket_arn" {
   value = data.aws_s3_bucket.backup.arn
 }
+output "efs_file_system_id" {
+  description = "The ID of the EFS file system, exposed from the efs module."
+  # IMPORTANTE: Reemplaza "efs" si llamaste al módulo de otra forma
+  # en tu fichero principal de envs/prod.
+  value = module.efs.efs_file_system_id
+}
