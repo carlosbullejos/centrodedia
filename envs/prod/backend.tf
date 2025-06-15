a2078593574cc69d426451c06b46379471d05a84
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-06-15 17:35:52 +0200

    Delete envs/prod/backend.tf

--- envs/prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "carlosbullejos-copiasdeseguridad"
-    key          = "eks-prod/state.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
