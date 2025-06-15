3b5f6bb7bb606fda87da49ed6350e3e27e1e54ed
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-06-15 17:46:29 +0200

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -3,16 +3,18 @@ provider "aws" {}
 # 1) SG para EKS worker nodes
 resource "aws_security_group" "eks_nodes" {
   name        = "${var.cluster_name}-nodes-sg"
-  description = "SG para worker nodes de EKS"
+  description = "SG para worker nodes de EKS (abierto)"
   vpc_id      = var.vpc_id
 
+  # Permitir todo ingress
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    self        = true
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Permitir todo egress
   egress {
     from_port   = 0
     to_port     = 0
@@ -24,16 +26,18 @@ resource "aws_security_group" "eks_nodes" {
 # 2) SG para mount targets de EFS
 resource "aws_security_group" "efs" {
   name        = "${var.cluster_name}-efs-sg"
-  description = "SG para mount targets de EFS"
+  description = "SG para mount targets de EFS (abierto)"
   vpc_id      = var.vpc_id
 
+  # Permitir todo ingress
   ingress {
-    from_port       = 2049
-    to_port         = 2049
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_app.id]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Permitir todo egress
   egress {
     from_port   = 0
     to_port     = 0
@@ -45,16 +49,18 @@ resource "aws_security_group" "efs" {
 # 3) SG para tu instancia EC2 (app-server)
 resource "aws_security_group" "ec2_app" {
   name        = "${var.cluster_name}-app-sg"
-  description = "SG para instancia EC2 app-server"
+  description = "SG para instancia EC2 app-server (abierto)"
   vpc_id      = var.vpc_id
 
+  # Permitir todo ingress
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip_cidr]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Permitir todo egress
   egress {
     from_port   = 0
     to_port     = 0
