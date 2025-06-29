95cf8d286b82d682568169b527ecaf93b4f5f52c
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2025-06-29 11:27:01 +0200

    Update backend.tf

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "carlosbullejos-copiasdeseguridad"
+    bucket       = "centrodia-backups"
     key          = "eks-prod/state.tfstate"
     region       = "us-east-1"
     use_lockfile = true
